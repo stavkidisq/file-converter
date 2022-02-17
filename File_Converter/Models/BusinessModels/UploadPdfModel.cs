3faddf786b55d732365ed5f5a055eaf06a0bdb81
stavkidisq <stavkidisq@gmail.com>
2022-02-17 21:36:32 +0300

    #5 Added BusinessModels/ and ValidationAttributes/. Added validation of upload files.

---
+++ File_Converter/Models/BusinessModels/UploadPdfModel.cs
@@ -0,0 +1,14 @@
+﻿using File_Converter.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace File_Converter.Models.BusinessModels
+{
+    public class UploadPdfModel
+    {
+        [Required(ErrorMessage = "Please select a file.")]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(10 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".pdf" })]
+        public IFormFile Information { get; set; }
+    }
+}
---
+++ File_Converter/Models/BusinessModels/UploadPowerPointModel.cs
@@ -0,0 +1,14 @@
+﻿using File_Converter.Models.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace File_Converter.Models.BusinessModels
+{
+    public class UploadPowerPointModel
+    {
+        [Required]
+        [DataType(DataType.Upload)]
+        [MaxFileSize(4 * 1024 * 1024)]
+        [AllowedExtensions(new string[] { ".pptx", ".ppt" })]
+        public IFormFile Information { get; set; }
+    }
+}
